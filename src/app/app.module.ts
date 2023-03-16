c5901f84d05ca95cb41c108a39ebd45208126536
prodalkimia <produccionesalkimiavzla@gmail.com>
2023-03-16 00:34:17 -0300

    Construcción parte de header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APLogoComponent } from './components/aplogo/aplogo.component';
+import { BanerComponent } from './components/baner/baner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APLogoComponent,
+    BanerComponent
   ],
   imports: [
     BrowserModule
